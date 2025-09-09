6e704bcc19064881b1837bf13b8c602e8f6aab5f
aadil-nv <153256798+aadil-nv@users.noreply.github.com>
2025-09-09 16:38:33 +0530

    Time: 10 ms (45.97%), Space: 58.8 MB (5.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,12 +1,12 @@
 var twoSum = function (nums, target) {
     let arr = nums.map((num, i) => [num, i]);
-    arr.sort((a, b) => a[0] - b[0]); // sort by values
+    arr.sort((a, b) => a[0] - b[0]);
 
     let left = 0, right = arr.length - 1;
     while (left < right) {
         let sum = arr[left][0] + arr[right][0];
         if (sum === target) {
-            return [arr[left][1], arr[right][1]]; // return original indices
+            return [arr[left][1], arr[right][1]]; 
         } else if (sum < target) {
             left++;
         } else {
